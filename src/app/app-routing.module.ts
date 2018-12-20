f755d799a0d308b28208c1944b72122c2e40dd2e
Adewole Feyijimi <feyijimi@tenforward.co>
2018-12-20 19:27:22 +0100

    tabs are working...create notabs 4 login et al

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,13 +8,29 @@ const routes: Routes = [
     pathMatch: 'full'
   },
   {
+    path: 'tab1',
+    redirectTo: 'list',
+    pathMatch: 'full'
+  },
+  {
+    path: 'tab2',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'tab3',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
     path: 'home',
     loadChildren: './home/home.module#HomePageModule'
   },
   {
     path: 'list',
     loadChildren: './list/list.module#ListPageModule'
-  }
+  },
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' }
 ];
 
 @NgModule({
